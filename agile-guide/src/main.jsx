662ce8b75193c31257c2eccf42a2c642cc22c148
Vikram Kanse <vikramkanse.nr@gmail.com>
2025-11-11 22:57:02 +0530

    updated basename in main js

--- agile-guide/src/main.jsx
+++ agile-guide/src/main.jsx
@@ -7,7 +7,7 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* 2. Wrap the entire application in BrowserRouter */}
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/agile-guide">
       <App/>
     </BrowserRouter>
   </React.StrictMode>,
